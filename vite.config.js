97fc6be8b680eb19ee7a841068828ef170660a85
Justin Kahshen Lu <justinklu75@gmail.com>
2023-08-27 19:02:23 -0400

    testing new changes

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  base: '/VuePortfolio2023/',
+  plugins: [ vue() ],
   resolve: {
     alias: {
       //'@': fileURLToPath(new URL('./src', import.meta.url))
       '@': fileURLToPath(new URL('./src', import.meta.url))
       //
-    },
-    base: "/VuePortfolio2023/"
+    }
   }
 })
